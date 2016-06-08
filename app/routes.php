a251e49a5dcfc7758e901f867e6d8385ff1c8278
Ramtane <rhidja@gmail.com>
2016-06-08 15:27:14 +0200

    Génération d'une routes

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,7 @@
 <?php
 // app/routes.php
 use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Response;
 
 $routes = new Routing\RouteCollection();
 $routes->add('hello', new Routing\Route('/hello/{name}', array(
@@ -15,6 +16,25 @@ $routes->add('hello', new Routing\Route('/hello/{name}', array(
         $response->headers->set('Content-Type', 'text/plain');
 
         return $response;
+    }
+)));
+
+function is_leap_year($year = null) {
+    if (null === $year) {
+        $year = date('Y');
+    }
+    
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
+    'year' => null,
+    '_controller' => function ($request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yep, this is a leap year!');
+        }
+
+        return new Response('Nope, this is not a leap year.');
     }
 )));
 
