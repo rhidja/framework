13958afeeea2d0d47ffd90b209b64e381fde5f00
Ramtane <rhidja@gmail.com>
2016-06-08 15:16:38 +0200

    Templating

--- app/routes.php
+++ app/routes.php
@@ -3,7 +3,19 @@
 use Symfony\Component\Routing;
 
 $routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'World')));
-$routes->add('bye', new Routing\Route('/bye'));
+$routes->add('hello', new Routing\Route('/hello/{name}', array(
+    'name' => 'World',
+    '_controller' => function ($request) {
+        // $foo will be available in the template
+        $request->attributes->set('foo', 'bar');
+
+        $response = render_template($request);
+
+        // change some header
+        $response->headers->set('Content-Type', 'text/plain');
+
+        return $response;
+    }
+)));
 
 return $routes;
