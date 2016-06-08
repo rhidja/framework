6f33ebcb022da4e4ba0ef54fc77895e6766280d8
Ramtane <rhidja@gmail.com>
2016-06-08 14:50:57 +0200

    Utilisation de symfony/rooting et addaptation du code

--- src/AppBundle/HelloController.php
+++ src/AppBundle/HelloController.php
@@ -1,4 +1,1 @@
-<!-- example.com/src/pages/hello.php -->
-<?php $name = $request->get('name', 'World') ?>
 
-Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
--- web/app.php
+++ web/app.php
@@ -5,21 +5,25 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing;
 
 $request = Request::createFromGlobals();
-$response = new Response();
+$routes = include __DIR__.'/../app/routes.php';
 
-$map = array(
-    '/hello' => __DIR__.'/../src/AppBundle/HelloController.php',
-    '/bye'   => __DIR__.'/../bye.php',
-);
+$context = new Routing\RequestContext();
+$context->fromRequest($request);
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-$path = $request->getPathInfo();
-if (isset($map[$path])) {
-    require $map[$path];
-} else {
-    $response->setStatusCode(404);
-    $response->setContent('Not Found');
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/AppBundle/%s.php', $_route);
+
+    $response = new Response(ob_get_clean());
+} catch (Routing\Exception\ResourceNotFoundException $e) {
+    $response = new Response('Not Found', 404);
+} catch (Exception $e) {
+    $response = new Response('An error occurred', 500);
 }
 
 $response->send();
