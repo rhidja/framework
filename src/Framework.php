c3275aff21076225440cf37c292a6c69e11cc38e
Ramtane Hidja <ramtane.hidja@smile.fr>
2021-12-09 14:50:57 +0100

    Refactor Framework class

--- src/Framework.php
+++ src/Framework.php
@@ -3,8 +3,36 @@ declare(strict_types=1);
 
 namespace App;
 
-use Symfony\Component\HttpKernel\HttpKernel;
+use App\EventListener\ContentLengthListener;
+use App\EventListener\GoogleListener;
+use App\EventListener\StringResponseListener;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel;
+use Symfony\Component\Routing;
 
-class Framework extends HttpKernel
+class Framework extends HttpKernel\HttpKernel
 {
+    public function __construct($routes)
+    {
+        $context = new Routing\RequestContext();
+        $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+        $requestStack = new RequestStack();
+
+        $controllerResolver = new HttpKernel\Controller\ControllerResolver();
+        $argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+
+        $eventDispatcher = new EventDispatcher();
+        $eventDispatcher->addSubscriber(new HttpKernel\EventListener\ErrorListener(
+            'App\Controller\ErrorController::exception'
+        ));
+        $eventDispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
+        $eventDispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
+        $eventDispatcher->addSubscriber(new StringResponseListener());
+        $eventDispatcher->addSubscriber(new ContentLengthListener());
+        $eventDispatcher->addSubscriber(new GoogleListener());
+        $eventDispatcher->addSubscriber(new StringResponseListener());
+
+        parent::__construct($eventDispatcher, $controllerResolver, $requestStack, $argumentResolver);
+    }
 }
--- web/app.php
+++ web/app.php
@@ -3,38 +3,15 @@ declare(strict_types=1);
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-use App\EventListener\StringResponseListener;
 use App\Framework;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpKernel\Controller;
-use App\EventListener\ContentLengthListener;
-use App\EventListener\GoogleListener;
-use Symfony\Component\Routing;
-use Symfony\Component\HttpKernel;
 
 $request = Request::createFromGlobals();
 $requestStack = new RequestStack();
 $routes = include __DIR__ . '/../config/routes.php';
 
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-
-$controllerResolver = new Controller\ControllerResolver();
-$argumentResolver = new Controller\ArgumentResolver();
-
-$eventDispatcher = new EventDispatcher();
-$eventDispatcher->addSubscriber(new HttpKernel\EventListener\ErrorListener(
-    'App\Controller\ErrorController::exception'
-));
-$eventDispatcher->addSubscriber(new ContentLengthListener());
-$eventDispatcher->addSubscriber(new GoogleListener());
-$eventDispatcher->addSubscriber(new StringResponseListener());
-$eventDispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
-$eventDispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
-
-$framework = new Framework($eventDispatcher, $controllerResolver, $requestStack, $argumentResolver);
+$framework = new Framework($routes);
 $response = $framework->handle($request);
 
 $response->send();
