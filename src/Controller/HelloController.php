8ba9dcc3356e2bfb9cf09459f4d7521e541c3c74
Ramtane Hidja <ramtane.hidja@smile.fr>
2021-12-12 13:33:58 +0100

    Install and implement templating component

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -5,11 +5,21 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Templating\Helper\SlotsHelper;
+use Symfony\Component\Templating\Loader\FilesystemLoader;
+use Symfony\Component\Templating\PhpEngine;
+use Symfony\Component\Templating\TemplateNameParser;
 
 class HelloController
 {
     public function helloAction(Request $request, $name)
     {
-        return new Response('Hello, this is my first test, '.$name);
+        $filesystemLoader = new FilesystemLoader(__DIR__.'/../../templates/views/%name%');
+
+        $templating = new PhpEngine(new TemplateNameParser(), $filesystemLoader);
+        $templating->set(new SlotsHelper());
+
+        return new Response($templating->render('hello.html.twig', ['firstname' => 'Fabien']));
+        //return new Response('Hello, this is my first test, '.$name);
     }
 }
