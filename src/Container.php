ccd52e6e24705d83c974404588c0809602b9cff4
Ramtane Hidja <ramtane.hidja@smile.fr>
2021-12-12 16:49:59 +0100

    Setup the app services in yaml files

--- src/Container.php
+++ src/Container.php
@@ -3,47 +3,23 @@ declare(strict_types=1);
 
 namespace App;
 
-use Symfony\Component\DependencyInjection;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\EventDispatcher;
-use Symfony\Component\HttpFoundation;
-use Symfony\Component\HttpKernel;
 use Symfony\Component\Routing;
+use Symfony\Component\Routing\RouteCollection;
 
 class Container
 {
-    public static function register($routes)
+    public static function register(RouteCollection $routes): ContainerBuilder
     {
-        $containerBuilder = new DependencyInjection\ContainerBuilder();
-        $containerBuilder->register('context', Routing\RequestContext::class);
+        $containerBuilder = new ContainerBuilder();
+        $loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__ . '/../config/'));
+        $loader->load('services.yaml');
+
         $containerBuilder->register('matcher', Routing\Matcher\UrlMatcher::class)
             ->setArguments([$routes, new Reference('context')])
-        ;
-        $containerBuilder->register('request_stack', HttpFoundation\RequestStack::class);
-        $containerBuilder->register('controller_resolver', HttpKernel\Controller\ControllerResolver::class);
-        $containerBuilder->register('argument_resolver', HttpKernel\Controller\ArgumentResolver::class);
-
-        $containerBuilder->register('listener.router', HttpKernel\EventListener\RouterListener::class)
-            ->setArguments([new Reference('matcher'), new Reference('request_stack')])
-        ;
-        $containerBuilder->register('listener.response', HttpKernel\EventListener\ResponseListener::class)
-            ->setArguments(['%charset%'])
-        ;
-        $containerBuilder->register('listener.exception', HttpKernel\EventListener\ErrorListener::class)
-            ->setArguments(['App\Controller\ErrorController::exception'])
-        ;
-        $containerBuilder->register('dispatcher', EventDispatcher\EventDispatcher::class)
-            ->addMethodCall('addSubscriber', [new Reference('listener.router')])
-            ->addMethodCall('addSubscriber', [new Reference('listener.response')])
-            ->addMethodCall('addSubscriber', [new Reference('listener.exception')])
-        ;
-        $containerBuilder->register('framework', Framework::class)
-            ->setArguments([
-                new Reference('dispatcher'),
-                new Reference('controller_resolver'),
-                new Reference('request_stack'),
-                new Reference('argument_resolver'),
-            ])
         ;
 
         return $containerBuilder;
